c5467db26c0d3f0856169acb6d5679e9fb53804c
DenizOzal <semih.ozal@ug.bilkent.edu.tr>
2022-03-03 19:50:14 +0000

    son hali çalışmıyo sikeyim

--- histclient.c
+++ histclient.c
@@ -5,47 +5,35 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
-
 #include "shareddefs.h"
 int main( int argc, char* argv[])
-{
-    mqd_t mq;
+{ 
+    mqd_t client_server;
+	mqd_t server_client;
     struct item item;
 	int n;
+	client_server = mq_open(CLIENT_SERVER, O_RDWR); // send
+	//server_client = mq_open(SERVER_CLIENT, O_RDWR | O_CREAT, 0666, NULL); // receive
 
-	mq = mq_open(MQNAME, O_RDWR);
-   
     char *p;
     int intervalCount = strtol(argv[1], &p, 10);
     int intervalWidth = strtol(argv[2], &p, 10);
     int intervalStart = strtol(argv[3], &p, 10);
 
-     if (mq == -1) {
-		perror("can not open msg queue\n");
-		exit(1);
-	}
-	printf("mq opened, mq id = %d\n", (int) mq);
+	item.intCount = intervalCount;
+    item.intWidth = intervalWidth;
+    item.intStart = intervalStart;
+	item.value = 0;
+	item.id = 0;
 
-		item.intCount = intervalCount;
-        item.intWidth = intervalWidth;
-        item.intStart = intervalStart;
-		n = mq_send(mq, (char *) &item, sizeof(struct item), 0);
 
-		if (n == -1) {
-			perror("mq_send failed\n");
-			exit(1);
-		}
-
-		printf("mq_send success, item size = %d\n",
-		       (int) sizeof(struct item));
-
-		//printf("item->id  = %d\n", item.id);
-		printf("item->intCount  = %d\n", item.intCount);
-		printf("item->intWidth = %d\n", item.intWidth);
-        printf("item->intStart = %d\n", item.intStart);
-		printf("\n");
+	n = mq_send(client_server, (char *) &item, sizeof(struct item), 0);
+	printf("item->intCount  = %d\n", item.intCount);
+	printf("item->intWidth = %d\n", item.intWidth);
+    printf("item->intStart = %d\n", item.intStart);
+	printf("\n");
 	
-	mq_close(mq);
-
+	mq_close(server_client);
+	mq_close(client_server);
 	return 0;
 }
--- shareddefs.h
+++ shareddefs.h
@@ -1,12 +1,10 @@
 struct item {
-	int id;
+	pid_t id;
 	int intCount;
 	int intWidth;
 	int intStart;
-	pid_t pid;
+	int value;
 };
-
-struct item_arr{
-	int histogram_data[1000];
-};
-#define MQNAME "/justaname"
+#define SERVER_CLIENT "/server_client"
+#define CLIENT_SERVER "/client_server"
+#define CHILD_PARENT "/child_parent"
